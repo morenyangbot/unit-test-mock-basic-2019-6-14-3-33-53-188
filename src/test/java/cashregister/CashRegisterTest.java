adfc1ece5d2303fc68369ef598721f35b8218195
Teng YANG <teng.yang@oocl.com>
2019-07-15 11:59:24 +0800

    test: Implement all test cases

--- src/test/java/cashregister/CashRegisterTest.java
+++ src/test/java/cashregister/CashRegisterTest.java
@@ -10,23 +10,43 @@ public class CashRegisterTest {
 
     @Test
     public void should_print_the_real_purchase_when_call_process() {
+
         //given
+        Printer mockPrinter = mock(Printer.class);
+        CashRegister cashRegister = new CashRegister(mockPrinter);
+
         //when
+        cashRegister.process(new Purchase(new Item[]{}));
+
         //then
+        verify(mockPrinter, times(1)).print("");
+        verify(mockPrinter, never()).print("Something cool");
     }
 
     @Test
     public void should_print_the_stub_purchase_when_call_process() {
         //given
+        Printer mockPrinter = mock(Printer.class);
+        CashRegister cashRegister = new CashRegister(mockPrinter);
+        Purchase purchase = mock(Purchase.class);
+        when(purchase.asString()).thenReturn("");
         //when
+        cashRegister.process(purchase);
         //then
+        verify(mockPrinter, times(1)).print("");
+        verify(mockPrinter, never()).print("Something cool");
     }
 
     @Test
     public void should_verify_with_process_call_with_mockito() {
         //given
+        Purchase purchase = mock(Purchase.class);
+        when(purchase.asString()).thenReturn("");
+        CashRegister cashRegister = mock(CashRegister.class);
         //when
+        cashRegister.process(purchase);
         //then
+        verify(cashRegister, times(1)).process(purchase);
     }
 
 }
